c47cf41c6d0cfb5006c9c70c0257b48d00c8419e
gsspdev <108854023+gsspdev@users.noreply.github.com>
2023-04-02 21:13:54 +0000

    init commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+let dice_roll = 9;
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    // other => move_player(other),
+    // _ => reroll(), // _ is a catch-all
+    _ => (), // this is a no-op
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+// fn move_player(num_spaces: u8) {}
+// fn reroll() {}
